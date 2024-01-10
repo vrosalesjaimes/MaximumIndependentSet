1c7f6045fd031fb0c742dda2ae2dff08ba3ae5ae
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2024-01-09 23:50:35 -0600

    Agregando metodo para escritura de graficas en formato para GraphViz

--- src/main/java/com/vrj/coh/mis/Graph.java
+++ src/main/java/com/vrj/coh/mis/Graph.java
@@ -1,6 +1,8 @@
 
 package com.vrj.coh.mis;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
@@ -8,13 +10,30 @@ public class Graph {
     private int[][] adjMatrix;
     private int numEdges;
 
-    public Graph(String graph6){
+    public Graph(String graph6) {
         Graph6Decoder decoder = new Graph6Decoder(graph6);
         this.adjMatrix = decoder.getAdjMatrix();
         this.numEdges = decoder.getNumEdges();
     }
 
-    public Graph subGraph(int[][] bitMap){
-        return null;
+    public String toString(List<Integer> vertexSet){
+        StringBuilder dot = new StringBuilder();
+        dot.append("graph G {\n");
+
+        for (int i = 0; i < adjMatrix.length; i++) {
+            dot.append(String.format("  %d [color=\"%s\"]\n", i, vertexSet.contains(i) ? "blue" : "white"));
+        }
+
+        for (int i = 0; i < adjMatrix.length; i++) {
+            for (int j = i + 1; j < adjMatrix.length; j++) {
+                if (adjMatrix[i][j] == 1) {
+                    String edgeColor = (vertexSet.contains(i) && vertexSet.contains(j)) ? "yellow" : "black";
+                    dot.append(String.format("  %d -- %d [color=\"%s\"]\n", i, j, edgeColor));
+                }
+            }
+        }
+
+        dot.append("}\n");
+        return dot.toString();
     }
 }
